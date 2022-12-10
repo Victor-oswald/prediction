56c8db58dd8665b69880a60edba19896f91e4b02
Victor-oswald <114114292+Victor-oswald@users.noreply.github.com>
2022-12-10 02:09:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+     <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HGGJ8ECMC5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HGGJ8ECMC5');
+</script> <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4920651646830729"
+crossorigin="anonymous"></script>
+    <meta name="description" content="Get match predictions, club analysis and lots more for free,"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keyword"
+        content="betting; lottery; prediction; bet; football betting; sure prediction; game prediction; football correct score
+        football; football home and away games; betting odds; betting sites; full time scores; match fixtures; kick off; playoffs;" />
+    <title>OSWALD</title>
+    <link rel="icon" type="image/x-icon" href="OSWALD.jpg" />
+    <link rel="stylesheet" href="home.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fixtures.css"/>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <span class="menu" style="font-size:100%; cursor:pointer" onclick="openNavigation()">
+                <div class="menu1"></div>
+                <div class="menu2"></div>
+                <div class="menu3"></div></span>
+            <a href="predictions.html" target="_blank">Predictions</a>
+            <a href="analysis.html" target="_blank">Analysis</a>
+            <a href="news.html" target="_blank">News</a>
+        </div>
+        </br></br></br>
+        <div id="mySidebar" class="sidebar">
+            <h1 style="color:blue; font-size:xx-large">MENU</h1></br></br><br>
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNavigation()">&times;</a>
+            <button id="predbtn" onclick="Prediction()">highlights</button></br></br>
+            <button type="button" onclick="table()" id="htbbtn">Tables</button></br></br>
+            <div class="htbbtn-content" id="table-content">
+                <a href="premier league tables.html">premier league</a></br></br>
+                <a href="serie a table.html">Serie A table</a></br>
+                <a href="Top goal scorers.html"> Top goal scorers</a></br>
+                <a href="champions league table.html">Champions League</a>
+            </div>
+            <button id="hfbtn" onclick="fixtures()">Fixtures</button></br></br>
+            <div class="hfbtn-content" id="fixture-content">
+                <a href="fixtures.html">Serie A </a></br></br>
+                <a href="premier league.html">Premier league</a></br>
+                <a href="champions league fixtures.html">Champions league</a></br></br>
+                <a href="laliga.html">Laliga </a></br></br>
+            </div>
+            <button id="abtbtn" onclick="About()">About us</button>
+        </div>
+        <script>
+            function openNavigation() {
+                document.getElementById("mySidebar").style.width = "40%";
+            }
+            function closeNavigation() {
+                document.getElementById("mySidebar").style.width = "0px";
+            }
+            document.getElementById("mySidebar").style.width="0px";
+
+            function table() {
+                var tmenu = document.getElementById("table-content");
+                var tbutton = document.getElementById("htbbtn")
+                if (tmenu.style.display == "none") {
+                    tmenu.style.display = "inline-block"
+                } else {
+                    tmenu.style.display = "none"
+                }
+            }
+            function fixtures() {
+                var fmenu = document.getElementById("fixture-content")
+                if (fmenu.style.display == "none") {
+                    fmenu.style.display = "block"
+                } else {
+                    fmenu.style.display = "none"
+                }
+            }
+            function Prediction(){
+            var prediction=document.getElementById("predbtn");
+            prediction=window.location.href="highlights.html";}
+            function About(){
+var about=document.getElementById("abtbtn");
+about=window.location.href="about us.html";}
+const result=[
+    "scores", "predictions", "fixtures","table","news","About"
+]
+var x=document.getElementById("search").value;
+var y=x.value;
+for(var i=0; i<result.length; ++i){
+    if(y==[i]){
+        document.getElementById("search-result").style.display="block";
+    }
+}
+        </script>
+
+
+        <div class="page">
+            <center></br></br>
+                <div style="background-color:maroon; height:auto; width:auto">
+                    <marquee><h2>Welcome To The Home Of Champions!</h2></marquee>
+                <img src="uefa.jpeg" style="width:70px; height:70px; border-radius: 30px; margin-left: 20px;"/>&nbsp; &nbsp; &nbsp;&nbsp;<img src="premier league.jpg" style="width:70px; height:70px;border-radius:30px ; margin-left: 30px;"/> &nbsp;&nbsp;&nbsp;&nbsp;<img src="europa.jpeg" style="width:70px; height:70px; border-radius:30px; margin-left:40px"/>
+                </div>
+                <div style="background-color:#333;">
+                    <h4 style="color:#ffff">Your Journey to Football world starts here!</h4>
+                </div>
+                <div>
+                    <img src="0_World-Cup-2022.jpg" style="width:100%; height: auto;" alt="Get Ready, The World Cup has Just Begun"/>
+                    <h1>WELCOME TO QATAR 2022, THE WORLD CUP HAS JUST BEGUN.</h1>
+                    <h3>The long awaited FIFA world Cup is finally here and the group stage competition kicks  off from sunday,20th of november 2022.<br>
+                        The competition will host a total number of 32 countries/teams ho have qaulified for the competition, the competion will last for<br> 
+                        a month in the hosts country(QATAR) and it comes in stages,that is the group stage and the knock out stage.<br><br><br>
+
+                        The world cup was first played in 1930 and since then has become the dream of every footballer and also one of the most enjoyed football<br>
+                        competition as it involves the whole world.</h3>
+                        <h1>WILL FANS MISS THE OTHER LEAGUES?</h1>
+                        <h3>
+                            Well i don't think there is any reason to feel bad because of your favourite leagues including champions league, Europa league and others<br>
+                            since most of your favourite players will be coming to qatar to defend thier countries and you will really enjoy it seeing your favourite <br>
+                            team mates play against themselves in the tournament.i believe it is going to be a great fun and you are surely going to enjoy it.
+                        </h3>
+                </div>
+                </center>
+
+            <h1 class="fix">TRENDING FIXTURES FOR THE WEEK</h1>
+            </br></br></br>
+            <center>
+                <div style="background-color:grey;font-size:Larger">Friday, 9th December</div><br><br>
+                <button style="width: 100%; height:auto"><img src="croatia.png" style='width:100px;height:70px; float:left' /><h2>CROATIA</h2>&nbsp;&nbsp;&nbsp;<span style="float:right">16:00</span>&nbsp;&nbsp;&nbsp;<h2>BRAZIL</h2><img src="brazil.png" style='width:100px;height:70px; float:left'/></button><br><br><br><br>
+                 <button style="width: 100%; height:auto"><img src="netherland.png" style='width:100px;height:70px;float:left' /><h2>NETHERLANDS</h2>&nbsp;&nbsp;&nbsp;<span style="float:right">20:00</span>&nbsp;&nbsp;&nbsp;<h2>ARGENTINA</h2><img src="argentina.png"style='width:100px;height:70px; float:left';/></button><br><br><br><br>
+    
+            </center>
+            <div class="eng">
+            </div></br></br>
+            <button id="engbtn" onclick="viewAll()">view all</button><br><br>
+            <div id="engother">
+                <center>
+
+               <div style="background-color:grey;font-size:Larger">Saturday, 10th December</div><br><br>
+               <button style="width: 100%; height:auto"><img src="morocco.jpeg" style='width:100px;height:70px;float:left' /><h2>MOROCCO</h2>&nbsp;&nbsp;&nbsp;<span style="float:right">16:00</span>&nbsp;&nbsp;&nbsp;<h2>PORTUGAL</h2><img src="portugal.png"style='width:100px;height:70px; float:left';/></button><br><br><br><br>
+               <button style="width: 100%; height:auto"><img src="england.png" style='width:100px;height:70px;float:left' /><h2>ENGLAND</h2>&nbsp;&nbsp;&nbsp;<span style="float:right">20:00</span>&nbsp;&nbsp;&nbsp;<h2>FRANCE</h2><img src="france.png"style='width:100px;height:70px; float:left';/></button><br><br><br><br>
+            </center>
+            </div>
+            <center> <h1>NEWS THAT YOU MAY NOT WANT TO MISS</h1> </center>
+            <div></br></br></br>
+                <button style="width:100%; height:auto"><img src="man u.jpeg" style="width:300px; float:left"/><br>
+                        <h3>Several epl players have recieved man of the match award in qatar so far and others are still recieving</h3><br><br>
+                    <a href="epl.html">Continue reading</a>
+                    </button><br><br>
+               <button style="width:100%; height:auto"><img src="images.jpeg.jpg" style="width:300px; float:left"/></br>
+                <h3>After joining Manchester city from Dortmund, the Norway  striker has maintained a very perfect record in the EPL, he has been making histories and breaking histories as well.</h3><br><br>
+                <a href="news.html">Continue Reading</a></button><br><br>
+                <button style="width:100%; height:auto"><img src="german protest.jpeg" style="width:300px; float:left"/><br>
+                <h3>EDEN HAZARD:We Are Here To Play Football</h3><br><br>
+                <a href="german protest.html">Continue reading</a>
+                </button>
+            </div></br></br>
+        <h2>Daily tips</h2>
+        <table class="anatable">
+      <thead>
+        <th>Fixtures</th>
+        <th>TIP</th>
+      </thead>
+      <tbody>
+        <tr>
+            <td>morocco<br>portugal</td>
+            <td>First Half<br>Draw</td>
+        </tr>
+        <tr>
+            <td>England<br>France</td>
+            <td>Goal-goal </td>
+        </tr>
+      </tbody>
+        </table>
+        </br>
+        <button><a href='predictions.html'>See More</a></button><br><br>
+        <div>
+            <h3><em>Hear What People Are Saying About Us</em></h3><br><br>
+            <h5 id="com"></h5>
+        </div>
+        
+                <div class="Foot"><br><br><br>
+                    <h1>Subscribe To Our Newsletter</h1> <br><br>  
+                <form >
+                    <input type="text" placeholder="Email" name="email"style='width:300px; height:30px' required/><br><br><br>
+                    <input type="submit" name="submit" value="submit"/>
+                </form>  <br><br><br>  
+                   <a href="https://www.facebook.com/profile.php?id=100066426476006" class="fa fa-facebook"></a>
+                    <a href="t.me/oswaldsoccer" class="fa fa-telegram"></a>
+                    <a href="https://twitter.com/VictorOswald01?t=KWsu3yQsFczxfyA5XNSGGg&s=01" class="fa fa-twitter"></a>
+                    <a href="https://youtube.com/channel/UCq-o4MyDCushNHS9VONNRiQ" class="fa fa-youtube"></a>
+                
+<a href="about us.html"><h2>Terms of use</h2></a>
+<a href="about us.html"><h2>privacy Policy</h2></a>
+<a href="about us.html"><h2>About us</h2></a>
+<form method="post" action="$_PHP_SELF">
+<textarea rows="6" cols="30" name="comment" >Comment</textarea><br><br>
+<button type="submit" name="com">Comment</button>
+</form>
+                    <h1 class="end1">&#169;: oswald 2022|| All right reserved</h1>
+                    </div>
+                </div></br></br>
+            <script>
+                document.getElementById("engother").style.display = "none";
+                function viewAll() {
+                    var more =
+                        document.getElementById("engother");
+                    var btnctrl =
+                        document.getElementById("engbtn");
+                    if (more.style.display === "none") {
+                        more.style.display = "block";
+                        btnctrl.innerHTML = "view less"
+                    } else {
+                        more.style.display = "none";
+                        btnctrl.innerHTML = "view all";
+                    }
+                };
+            </script>
+            </div>
+            </div>
+            <?php
+$com=$_POST['comment'];
+if(isset($_POST['com'])||$com!==null){
+echo $com;
+}else{
+    echo'<script type="text/javascript">.prompt("Please Enter Your Comment");.</script>.';
+}
+            ?>
+            </div>
+</body>
+
+</html>
